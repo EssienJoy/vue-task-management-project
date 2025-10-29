ad47968c4977a09fd42b6caff64c1e8670888006
essienjoy <ejoy7602@gmail.com>
2025-10-29 04:39:20 +0100

    updated signup form

--- src/auth/signUp.js
+++ src/auth/signUp.js
@@ -1,10 +1,20 @@
 export async function signUpApi(userData) {
-    return await fetch("http://localhost:3000/users", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(userData),
-    });
+    try {
+        const res = await fetch("http://localhost:3000/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(userData),
+        });
 
+
+        return true;
+    } catch (err) {
+        throw new Error(
+            err.message === "Failed to fetch"
+                ? "❌ Account Could Not be Created"
+                : err.message
+        );
+    }
 }
--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,41 +1,101 @@
+<script setup>
+	import { reactive } from "vue";
+	import Input from "../ui/Input.vue";
+	import Label from "../ui/Label.vue";
+	import { signUpApi } from "../auth/signUp.js";
+	import { useToast } from "vue-toastification";
+	import { useMutation } from "@tanstack/vue-query";
+	import { useRouter } from "vue-router";
+
+	const toast = useToast();
+	const router = useRouter();
+
+	const form = reactive({
+		userName: "",
+		email: "",
+		password: "",
+		confirmPassword: "",
+	});
+
+	const { mutate: signUp, isPending } = useMutation({
+		mutationFn: signUpApi,
+		onSuccess: () => {
+			router.push("/login");
+			toast.success("Account Successfully Created ✅");
+		},
+		onError: (err) => {
+			toast.error(err.message);
+		},
+	});
+
+	function handleSignUp() {
+		if (form.password !== form.confirmPassword) {
+			toast.error("Password does not match");
+			return;
+		}
+
+		if (form.password.length < 4) {
+			toast.error("Password must be 4 digit numbers or letters");
+			return;
+		}
+
+		if (form.userName.length < 3) {
+			toast.error("Username must not be less than 3 letters");
+			return;
+		}
+
+		if (form.email.includes("@") && form.email.includes(".com")) {
+			signUp({
+				userName: form.userName,
+				email: form.email,
+				password: form.password,
+				role: "user",
+			});
+		} else {
+			toast.error("❌ Incorrect, email must include @ and .com");
+		}
+	}
+</script>
 <template>
-	<form className="flex flex-col gap-4">
+	<form @submit.prevent="handleSignUp" className="flex flex-col gap-4">
 		<div>
 			<Label>User Name</Label>
-			<Input type="text" placeholder="Enter your name" />
+			<Input
+				v-model="form.userName"
+				type="text"
+				placeholder="Enter your name" />
 		</div>
 
 		<div>
 			<Label className="block text-sm font-medium text-gray-700 mb-1">
 				Email
 			</Label>
-			<Input type="email" placeholder="Enter your email" />
+			<Input v-model="form.email" type="email" placeholder="Enter your email" />
 		</div>
 
 		<div>
 			<Label>Password</Label>
 			<Input
 				type="password"
-				placeholder="Create a
-			password" />
+				v-model="form.password"
+				placeholder="Create a password" />
 		</div>
 
 		<div>
 			<Label>Confirm Password</Label>
-			<Input type="password" placeholder="Confirm your password" />
+			<Input
+				type="password"
+				placeholder="Confirm your password"
+				v-model="form.confirmPassword" />
 		</div>
 
 		<button
 			type="submit"
-			className="w-full bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 rounded-lg transition">
-			Sign Up
+			className="w-full bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 rounded-lg transition"
+			:disabled="isPending">
+			{{ isPending ? "Signing Up" : "Sign Up" }}
 		</button>
 	</form>
 </template>
-
-<script setup>
-	import Input from "../ui/Input.vue";
-	import Label from "../ui/Label.vue";
-</script>
 
 <style></style>
